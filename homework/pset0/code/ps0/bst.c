ec20f2ea3a394afd98bc4d9d7db1ab27b056ec1a
Luis Antonio Perez <luis0@myth66.stanford.edu>
2019-04-07 14:10:31 -0700

    Started code

---
+++ homework/pset0/code/ps0/bst.c
@@ -0,0 +1,33 @@
+#include "bst.h"
+#include <stddef.h>  // For NULL
+#include <stdlib.h>  // For malloc, free
+
+void insert_into(struct Node** root, int value) {
+  /* TODO: Implement this function! */
+  (void) root;
+  (void) value;
+}
+
+void free_tree(struct Node* root) {
+  /* TODO: Implement this function! */
+  (void) root;
+}
+
+size_t size_of(const struct Node* root) {
+  /* TODO: Implement this function! */
+  (void) root;
+  return 0;
+}
+
+int* contents_of(const struct Node* root) {
+  /* TODO: Implement this function! */
+  (void) root;
+  return NULL;
+}
+
+const struct Node* second_min_in(const struct Node* root) {
+  /* TODO: Implement this function! */
+  (void) root;
+  return NULL;
+}
+
